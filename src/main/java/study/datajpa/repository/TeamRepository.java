df4e9f4c2a349b2d457fc4f07f9aea56d9ddd09c
Jaewoo Kim <wodndhksk@gmail.com>
2023-08-28 20:20:30 +0900

    공통 인터페이스 설정

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,39 +1,8 @@
 package study.datajpa.repository;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Team;
 
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    EntityManager em;
-
-    public Team save(Team team){
-        em.persist(team);
-        return team;
-    }
-
-    public void remove(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll(){
-        return em.createQuery("select t from Team t", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id){
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count(){
-        return em.createQuery("select count(t) from Team t", Long.class)
-                .getSingleResult();
-    }
+//@Repository //생략가능
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
