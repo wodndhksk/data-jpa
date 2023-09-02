b9dcc284621e721bd15e35781fee8929d2e8be45
Jaewoo Kim <wodndhksk@gmail.com>
2023-09-02 16:14:39 +0900

    메소드 이름으로 쿼리 생성
    - 스프링 데이터 jpa 에서 메소드 이름으로 자동 쿼리 생성 (규칙 설명)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -57,9 +57,26 @@ class MemberRepositoryTest {
         memberRepository.delete(member2);
         long deletedCount = memberRepository.count();
         assertThat(deletedCount).isEqualTo(0);
-
-
     }
 
+    @Test
+    public void findByUsernameAndAgeGreaterThan(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findHelloBy() {
+        List<Member> top3MemberBy = memberRepository.findTop3HelloBy();
+
+    }
 
 }
