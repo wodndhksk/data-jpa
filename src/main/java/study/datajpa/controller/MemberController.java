13f6d4246a96b3584b843ee60b9af66d126c93b4
Jaewoo Kim <wodndhksk@gmail.com>
2023-09-25 21:47:05 +0900

    Web 확장 - 페이징과 정렬
    - 페이징 글로벌 세팅 (application.yml)
    - @PageableDefault 을 사용한 세팅
    - 주의!! : return은 Dto로 return 할것

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,9 +2,13 @@ package study.datajpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -37,8 +41,22 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * ex) http://localhost:8080/members?page=1&size=3
+     *  dhlqn
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 5,sort = "username") Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new); //Method reference 로 refactor
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("userA"));
+        for(int i=0; i<100; i++) {
+            memberRepository.save(new Member("user"+i, i));
+        }
     }
 }
